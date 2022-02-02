57e5e4561135c98e40ed4af0035a48574aff27c6
unknown <ivangonzalez0302@gmail.com>
2022-02-02 19:42:34 -0300

    Cambio numero 2

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,12 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Esto funciona de maravilla");
+var mensaje;
+
+
+mensaje = "Esto funciona de maravilla"
+
+
+	alert(mensaje);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -6,4 +6,3 @@ function mostrar()
 {
 	alert(respuesta);
 }
-
