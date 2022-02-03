fbc8dacf6ad2396b8ba6e087791ef4fa42b82778
unknown <ivangonzalez0302@gmail.com>
2022-02-02 23:57:36 -0300

    clase

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,7 +1,10 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-var respuesta = prompt ("Ingrese su nombre");
+var respuesta
+
+     respuesta = prompt ("Ingrese su nombre");
+
 function mostrar()
 {
 	alert(respuesta);
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -2,8 +2,14 @@
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
-function mostrar()
+var dato;
+
+	dato = prompt("Ingrese su nombre");
+
+	function mostrar()
 {
-	alert("ok");
+	
+	txtIdNombre.value = dato
+	//alert(dato);
 }
 
